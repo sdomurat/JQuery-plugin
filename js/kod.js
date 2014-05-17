59cf40f9dcc4d24db4fd2248261870d8447accc9
Szymon <dziara16@gmail.com>
2014-05-17 15:42:31 +0200

    pierwsza validacja imienia

--- js/kod.js
+++ js/kod.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
+	var isOk = function(object) {
+  //  $(object).attr( 'id', "" + "inputSuccess2" );
+    $(object).parent().addClass( "has-success" + " has-feedback" );
+    $(object).parent().append("<span></span>");
+    $(object).next().addClass('class', "glyphicon" + " glyphicon-ok" + " form-control-feedback");
+		alert("ok")
+	};
+
+		var isError = function(object) {
+		//	$(object).attr( 'id', "" + "inputError1" );
+			$(object).parent().addClass( "has-error" + " has-feedback" );
+			$(object).parent().append("<span></span>");
+			$(object).next().attr('class', "glyphicon" + " glyphicon-remove" + " form-control-feedback");
+			alert("nie")
+		};
+
 	$("#imie").focusout(function() {
 		$(this).validate({
-			lenghts: "4-9"
+			lenghts: "4-9",
+			showOk: isOk,
+			showError: isError
 		});
 	});
 });
--- js/plugin.js
+++ js/plugin.js
@@ -1,23 +1,40 @@
 (function($) {
 	$.fn.validate = function(settings) {
 		var settings = $.extend({
-			lenghts: "0-9999"
+			lenghts: "0-9999",
+			showOk: function(msg) {alert("Ok :)")},
+			showError: function(msg) {alert("Błąd -> nieprawidłowa ilość liter!")}
 		}, settings);
 
 		var lenghtsTest = settings.lenghts.split("-").map(function(number) {
 			return parseInt(number);
 		});
 		var value = $(this).val();
+		var isOk      = settings.showOk;
+		var isError      = settings.showError;
+
 		console.log("value " + value);
 		console.log("value.length " + value.length);
 		console.log("lenghtsTest.length " + lenghtsTest.length);
 		console.log("lenghtsTest[0] " + lenghtsTest[0]);
 		console.log("lenghtsTest[1] " + lenghtsTest[1]);
 
+		var error = 0;
 		//return this.each(function() {
 			if (lenghtsTest[0] !== value.length){
-				console.log("error");
+				console.log("error1111");
+				error = 1;
+
+			}
+			if (lenghtsTest[0] > value.length || value.length > lenghtsTest[1]){
+				error = 1;
+				console.log("error2222");
+				isError(this);
+
 			};
+			if (error = 0) {
+				isOk(this);
+			}
 		//})
 	};
 }(jQuery));
