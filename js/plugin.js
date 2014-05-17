653ae823f32c8598aac7f914ad4dbbad1de43286
Szymon <dziara16@gmail.com>
2014-05-17 16:11:05 +0200

    validacja imienia

--- js/plugin.js
+++ js/plugin.js
@@ -2,6 +2,7 @@
 	$.fn.validate = function(settings) {
 		var settings = $.extend({
 			lenghts: "0-9999",
+			pattern: new RegExp(/.*/),
 			showOk: function(msg) {alert("Ok :)")},
 			showError: function(msg) {alert("Błąd -> nieprawidłowa ilość liter!")}
 		}, settings);
@@ -9,6 +10,7 @@
 		var lenghtsTest = settings.lenghts.split("-").map(function(number) {
 			return parseInt(number);
 		});
+		var pattern       = settings.pattern;
 		var value = $(this).val();
 		var isOk      = settings.showOk;
 		var isError      = settings.showError;
@@ -21,20 +23,24 @@
 
 		var error = 0;
 		//return this.each(function() {
-			if (lenghtsTest[0] !== value.length){
-				console.log("error1111");
-				error = 1;
+		//	if (lenghtsTest[0] == value.length){
+		//		console.log("error1111");
+		//		error = 1;
+		//		isOk(this);
 
-			}
+		//	}
 			if (lenghtsTest[0] > value.length || value.length > lenghtsTest[1]){
 				error = 1;
 				console.log("error2222");
 				isError(this);
 
-			};
-			if (error = 0) {
+			} else{
 				isOk(this);
-			}
+			};
+			if (!pattern.test(value)) {
+				error = 1;
+				isError(this);
+			};
 		//})
 	};
 }(jQuery));
